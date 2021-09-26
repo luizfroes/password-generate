daed05fff5e03413f719935e26d1040235a81d7e
Luiz Carlos Froes <luizfroes@gmail.com>
2021-09-26 13:22:12 +0100

    Added the main functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // PLEASE ADD ALL YOUR CODE HERE
+
+  // Should return the password criteria
+  var getCriteria = function () {
+    return;
+  };
+
+  //Should return generatePassword()
+  var getRandomPassword = function () {
+    return;
+  };
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
