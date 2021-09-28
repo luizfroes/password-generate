2006d7e075481a0a0b4926f9ee4fe3eedf7a6016
Luiz Carlos Froes <luizfroes@gmail.com>
2021-09-28 14:57:51 +0100

    Get length value

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,44 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // PLEASE ADD ALL YOUR CODE HERE
-
-  // Should return the password criteria
-  const getCriteria = function () {
-    return;
-  };
-
-  //Should return generatePassword()
-  const getRandomPassword = function () {
-    return;
-  };
-
-  //Should return a random character from the picked array
-  const getRandomCharacter = function () {
-    return;
-  };
-
-  const userCriteria = {
-    length: 0,
-    isUppercase: true,
-    isLowercase: true,
-    isNumeric: true,
-    isSpecialCharacter: true,
-  };
+  return;
 }
+// PLEASE ADD ALL YOUR CODE HERE
+
+// Should return the password criteria
+const getLengthCriteria = function () {
+  const userCriteriaLength = prompt(
+    "Please choose the length of your password. (Between 8 and 128 characters)"
+  );
+  return userCriteriaLength;
+};
+
+// Validate the Length input true or false
+
+const validateLength = function (userCriteriaLength) {
+  return;
+};
+
+//Should return generatePassword()
+const getRandomPassword = function () {
+  return;
+};
+
+//Should return a random character from the picked array
+const getRandomCharacter = function () {
+  return;
+};
+
+const userCriteria = {
+  length: 0,
+  isUppercase: true,
+  isLowercase: true,
+  isNumeric: true,
+  isSpecialCharacter: true,
+};
+
+const userCriteriaLength = getLengthCriteria();
+console.log(userCriteriaLength);
 
 // Write password to the #password input
 function writePassword() {
