a6a6b1b23d72f3ac302fe0886cfb0a53eb0cb978
Luiz Carlos Froes <luizfroes@gmail.com>
2021-09-29 01:10:17 +0100

    Added arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,70 @@ const getRandomCharacter = function () {
   return;
 };
 
+//Declaring Arrays
+const lowercase = [
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  s,
+  t,
+  u,
+  v,
+  x,
+  w,
+  y,
+  z,
+];
+
+const uppercase = [
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  X,
+  W,
+  Y,
+  Z,
+];
+
+const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const specialCharacters = ['"', '#', '$', '%', '&', "'", '(', ""," ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\', ']', '^', '_', '`', '{', '|', '}', '~', '!',];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
